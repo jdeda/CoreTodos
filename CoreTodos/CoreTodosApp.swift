39df53811c886960a370fe122dd6edade3c3f4ad
jdeda <jldeda00@gmail.com>
2023-02-05 13:08:52 -0500

    Initial Commit

---
+++ CoreTodos/CoreTodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreTodosApp.swift
+//  CoreTodos
+//
+//  Created by Jesse Deda on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreTodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
