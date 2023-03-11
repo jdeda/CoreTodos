733542da1f3405833d6597da107f50c6ca377adc
jdeda <jldeda00@gmail.com>
2023-03-11 11:41:38 -0500

    initial

--- CoreTodos/Data/MockData.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-let mockTodos: [Todo] = [
-  .init(id: .init(), description: "Wakeup", isComplete: true),
-  .init(id: .init(), description: "Do homework", isComplete: false),
-  .init(id: .init(), description: "Play videogames", isComplete: true),
-  .init(id: .init(), description: "Do keto", isComplete: false),
-  .init(id: .init(), description: "Go to bed", isComplete: false),
-]
